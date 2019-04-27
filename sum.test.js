cd726237d3769ff07e553290fd540664ffb2cb01
Anjola <anjola@outlook.com>
2019-04-27 16:51:20 +0100

    added more tests

--- sum.test.js
+++ sum.test.js
@@ -8,11 +8,9 @@ test('adds 5 + 2 to equal 7', () => {
   expect(sum(5, 2)).toBe(7);
 });
 
-
 test('adds 6 + 2 to equal 8', () => {
   expect(sum(6, 2)).toBe(8);
 });
-
 
 test('adds 3 + 2 to equal 5', () => {
   expect(sum(3, 2)).toBe(5);
